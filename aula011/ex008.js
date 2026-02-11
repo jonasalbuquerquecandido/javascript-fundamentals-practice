459f4ecb6c44ab624a0f1ceaa7ea17f05c852783
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-02-11 16:12:58 -0300

    JavaScript Class

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do se carro é: ${vel} km/h`)
+if (vel > 60) { //Condição simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
