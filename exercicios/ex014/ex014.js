05d67d12640a0c5d7f1e1654e4aa91468efc1ed8
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-02-13 16:04:57 -0300

    JavaScript exercise completed

--- exercicios/ex014/ex014.js
+++ exercicios/ex014/ex014.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    
+    if (hora >= 0 && hora < 12) {
+        img.src = 'dia.png'
+        document.body.style.background = "#db9816"
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'tarde.png'
+        document.body.style.background = "#7f8455"
+    } else { 
+        img.src = 'noite.png'
+        document.body.style.background = '#1a2e2f'
+    }
+}
