c2700a98853be00b3869d70cf8defefc54a5e0fa
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-02-14 13:13:43 -0300

    created exercice 015

--- exercicios/ex015/ex015.js
+++ exercicios/ex015/ex015.js
@@ -2,17 +2,18 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 
     msg.innerHTML = `Agora são ${hora} horas.`
     
     if (hora >= 0 && hora < 12) {
-        img.src = 'dia.png'
+        img.src = 'image/adultohomem.png'
         document.body.style.background = "#db9816"
     } else if (hora >= 12 && hora <= 18) {
-        img.src = 'tarde.png'
+        img.src = 'image/jovemhomem.png'
         document.body.style.background = "#7f8455"
     } else { 
-        img.src = 'noite.png'
+        img.src = 'image/idosohomem.png'
         document.body.style.background = '#1a2e2f'
     }
 }
