f95e010c0f0da43bac616f98dc21a4312a91601d
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-03-11 09:46:58 -0300

    exercice in js

---
+++ exercicios/ex21/função01.js
@@ -0,0 +1,9 @@
+function parimp(n) {
+    if (n%2 == 0){
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(parimp(3)) // É NECESSÁRIO UMA CHAMADA PARA EXECUTAR A AÇÃO
---
+++ exercicios/ex21/função04.js
@@ -0,0 +1,8 @@
+// ENCONTRANDO O FATORIAL DE UM NÚMERO
+
+function fatorial(n) {
+   let fat = 1
+   for(let c = n; c > 1; c--) {
+    
+   }
+}
