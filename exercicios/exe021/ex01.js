5622cf4c41b4844c3a23074e5641449fe2909720
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-03-11 19:18:35 -0300

    Create ex01.js

---
+++ exercicios/exe021/ex01.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "banana", "orange"]
+
+console.log(`${fruits}`)
+
+let colors = ["red", "green", "blue", "yellow"]
+
+console.log(`${colors[0]} ${colors[2]}`)
+
+let numbers = [1, 2, 3]
+numbers.push(4)
