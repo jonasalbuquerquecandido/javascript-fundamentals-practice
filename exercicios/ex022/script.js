35abb58c2fc4d860079c198afed6eaf22cec4168
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-03-12 17:45:19 -0300

    exercice 22 completed

--- exercicios/ex022/script.js
+++ exercicios/ex022/script.js
@@ -26,8 +26,41 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) { // Verificar se o valor esta em numero ou lista
-        window.alert('Tudo ok!')
+            valores.push(Number(num.value))
+            let item = document.createElement('option')
+            item.text = `Valor ${num.value} adicionado`
+            lista.appendChild(item)
+            res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = '' // zera o conteúdo do input após executar a ação
+    num.focus() // Faz o ponteiro voltar para o espaço apos realizar a ação
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        // Analisar qual é maior e menor e colocar no console
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadrastados.</p>`
+        res.innerHTML += `<p>O maior valor adicionado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor adicionado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A media de todos os valores adicionados é ${media}.</p>`
     }
 }
