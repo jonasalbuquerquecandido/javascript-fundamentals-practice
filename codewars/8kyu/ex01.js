1e85f2db17ca9e7ba2f1506a2d6361e3240ae3f1
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-03-11 22:28:26 -0300

    exercices codwars

---
+++ codewars/8kyu/ex01.js
@@ -0,0 +1,10 @@
+// Retornar um valor boleano
+const boolToWord = (n) => n >= ? "yes" : "No"
+// Ou
+function boolToWord(n){
+    if(n){
+        return 'Yes'
+    } else {
+        return 'No'
+    }
+}
--- exercicios/exe021/ex01.js
+++ exercicios/exe021/ex01.js
@@ -1,10 +1,31 @@
+// Exercice 1
 let fruits = ["apple", "banana", "orange"]
-
 console.log(`${fruits}`)
-
+// Exercice 2
 let colors = ["red", "green", "blue", "yellow"]
-
 console.log(`${colors[0]} ${colors[2]}`)
-
+// Exercice 3
 let numbers = [1, 2, 3]
-numbers.push(4)
+numbers.push(4) //adiciona um novo elemento
+numbers.splice(0, 1) //remove o elemento
+numbers.sort() //ordena os elementos
+console.log(numbers)
+// Exercice 4
+let pets = ["cat", "dog", "bird"]
+console.log(pets.length) // Contar elementos
+// Exercice 5
+let names = ["Alice", "Bob", "Charlie"]
+for(let onlyname in names){ //cria um variável onlyname e coloca o valor de cada elemento de names nela, pulando linhas
+    console.log(names[onlyname])
+}
+// Exercice 6
+
+// Ecercice 7
+
+// Ecercice 8
+
+// Ecercice 9
+    let nums = [5, 10, 15, 20, 25]
+    let greaters = [nums>=10]
+    console.log(nums[greaters])
+// Ecercice 10
