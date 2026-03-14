62ab49a54e4227e22d9c9d370778027a856b91d2
Jonas Albuquerque Cândido <jonasalbuquerque.c@gmail.com>
2026-03-14 09:37:37 -0300

    Create pratica-variaveis.js

---
+++ viradev/pratica-variaveis.js
@@ -0,0 +1,12 @@
+// Declaração de Variaveis
+
+// Tipos primitivos
+const a = 5 // number
+cons b = "maça" // string
+cons  c = true  // bolean
+let u; //undefined
+
+// Primitivos adicionais
+const n = null
+const big = 123n
+const symb = Symbol('id')
